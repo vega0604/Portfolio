d976261fe776ea395997b4fd0a343b4d9a826571
Sebastian Vega <sebasvega4604@gmail.com>
2024-03-10 20:57:15 -0400

    added components and general layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from "@pages/Layout";
+import "@css/global.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Layout />}>
+          <Route path="" element={<Overview />}/>
+          <Route path="overview" element={<Overview />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="projects" element={<Projects />}/>
+          <Route path="socials" element={<Socials />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
