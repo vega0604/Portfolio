1dbbac4768028a5e6704038c4dc4017f7756f18f
Sebastian Vega <sebasvega4604@gmail.com>
2025-02-03 23:44:57 -0500

    feat(projects): added basic styles for projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,83 @@
-
+import { PROJECTS } from "@data/projects";
+import { COLLABORATORS } from "@data/collaborators";
+import { TOOLS, STATUS, PROJECT_TYPE } from "@data/enums";
+import { stackToString, stackToConicGradient, dateToString } from "@utils/converters";
+import { useState } from "react";
+import styles from "@styles/projects.module.css";
 
 function Projects(){
+    const [displayedProjects, setDisplayedProjects] = useState(PROJECTS);
+
     return (
-        <>
-        Projects
-        </>
+        <section className={styles.project_section}>
+            <search className={styles.form_container}>
+                <form>
+                    <div className={styles.searchbar_container}>
+                        <input type="text" />
+                        <div className={styles.filters_container}>
+                            <div>
+                                <h3>Date</h3>
+                                <ul>
+                                    
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>Status</h3>
+                                <ul>
+                                    
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>Type</h3>
+                                <ul>
+                                    
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>Tech</h3>
+                                <ul>
+                                    
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>Collaborators</h3>
+                                <ul>
+                                    
+                                </ul>
+                            </div> 
+                        </div>
+                    </div>
+                    <div className={styles.sorter_container}>
+                        <button></button>
+                        <div></div>
+                    </div>
+                </form>
+            </search>
+            <ul className={styles.project_list}>
+                {displayedProjects.map((project, i) => 
+                    <li key={i} className={styles.project_item}>
+                        <h2 className={styles.project_name}>{project.name}</h2>
+                        <p className={styles.project_dates}>{dateToString(project.start_date)} {project.end_date && `- ${dateToString(project.end_date)}`}</p>
+                        <p className={styles.project_description}>{project.description}</p>
+                        <div className={styles.development_info_container}>
+                            <div className={styles.tech_stack_container}>
+                                <div className={styles.piechart} style={{background: `conic-gradient(${stackToConicGradient(project.tech_stack)})`}}></div>
+                                <p>{stackToString(project.tech_stack)}</p>
+                            </div>
+                            <ul className={styles.collaborators_list}>
+                                {project.collaborators.map((person, j) => person.github && 
+                                    <li key={j} className={styles.collaborator}>
+                                        <a href={person.github} target="_blank">
+                                            <img src={`${person.github}.png`} alt="Collaborator GitHub" />
+                                        </a>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    </li>
+                )}
+            </ul>
+        </section>
     );
 }
 
--- src/utils/converters.js
+++ src/utils/converters.js
@@ -1,5 +1,5 @@
 
-export const stringToHex = (str) => {
+const stringToHex = (str) => {
     let hash = 0;
     str.split('').forEach(char => {
       hash = char.charCodeAt(0) + ((hash << 5) - hash)
@@ -10,4 +10,33 @@ export const stringToHex = (str) => {
       colour += value.toString(16).padStart(2, '0')
     }
     return colour;
+}
+
+export const stackToConicGradient = (stack) => {
+  const turn = 1 / stack.length;
+
+  let totalPercentage = 0;
+  let currentString = ""
+
+  for (let i=0;i<stack.length;i++){
+    totalPercentage += turn;
+    currentString += `${stringToHex(stack[i])} 0turn ${totalPercentage}turn, `;
+  }
+  return currentString.slice(0, currentString.length-2);
+}
+
+export const stackToString = (stack) => {
+  if (stack.length < 1) return "";
+  let string = "";
+  for (let i=0; i < stack.length-1; i++){
+    string += stack[i] + ", ";
+  }
+ 
+  string += stack[stack.length-1] + ".";
+  return string;
+}
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+export const dateToString = (date) => {
+  return `${months[date.getMonth()]} ${date.getFullYear()}`;
 }
