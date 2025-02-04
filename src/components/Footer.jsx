0a648583b5ce87a9ac210dadc99420ccc216d791
Sebastian Vega <sebasvega4604@gmail.com>
2025-02-04 00:12:04 -0500

    feat(footer): added resume link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,14 @@ function Footer(){
     return (
         <footer className={`${styles.footer} container`}>
             <p>© 2025 by Sebastian Vega. All rights reserved.</p>
-            <Link to='contact'>contact me</Link>
+            <ul className={styles.links_list}>
+                <li>
+                    <a href="/resume.pdf" target="_blank" className={styles.link}>resume</a>
+                </li>
+                <li>
+                    <Link to='contact' className={styles.link}>contact me</Link>
+                </li>
+            </ul>
         </footer>
     );
 }
